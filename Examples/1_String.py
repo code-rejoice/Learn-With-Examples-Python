8be8b628d108bfcb66eec64694f9c342721a6536
Code Rejoice <code.rejoice@gmail.com>
2017-07-23 11:03:24 -0400

    INITIAL COMMIT
    This is the initial commit for python examples project

---
+++ Examples/1_String.py
@@ -0,0 +1,129 @@
+#tutorials 3: Execute the below instructions in the interpreter
+
+#execute the below command
+"Hello World!"
+
+print("Hello World!")
+
+#execute using single quote
+'Hello World!'
+
+print('Hello World!')
+
+#use \' to escape single quote
+'can\'t'
+
+print('can\'t')
+
+#or use double quotes
+"can't"
+
+print("can't")
+
+#more examples 1- using print function
+print(
+	'" I won\'t," she said.'
+	)
+
+#more examples 2
+a = 'First Line. \n Second Line'
+print(a)
+
+#String concatenation
+print("\n-->string concatenation example without using +")
+print('Py''thon')
+
+#String concatenation using +
+print("\n-->string concatenation: concatenating two literals using +")
+print("Hi "+"there!")
+
+#String and variable concatenation using +
+print("\n-->string concatenation: concatenating two literals using +")
+a="Hello"
+print( a +" there!")
+
+#concatenation of two variables
+print("\n-->string concatenation: concatenating two String variables using +")
+b= " World!"
+print(a+b)
+
+# String indexing
+print("\n-->string Indexing: Declare a String variable and access the elements using index")
+word = "Python"
+
+print("\n--> word[0]")
+print(word[0])
+print("\n--> word[5]")
+print(word[5])
+
+print("\n--> word[-6]")
+print(word[-6])
+print("\n -->if you type word[7]], you would get an error as shown below, as it is out of range")
+print("  File \"<stdin>\", line 1")
+print("     word[7]]")
+print("          ^")
+print("SyntaxError: invalid syntax")
+print("	  \")")
+
+print("-->Notice the index for each of the letters in the word 'PYTHON' both forward and backward")
+print("\n  -->  P  Y  T  H  O  N")
+print("  -->  0  1  2  3  4  5")
+print("  --> -6 -5 -4 -3 -2 -1")
+
+
+print("--> word[0:4]")
+print(word[0:4])
+
+print("--> word[:2]")
+print(word[:2])
+
+
+print("--> word[4:]")
+print(word[4:])
+
+print("--> word[-4]")
+print(word[-4])
+
+print("--> word[2]='N' --> String is immutable and hence cannot be changed, if we try to, we would get the below error.But we can always create a new String")
+print("\nTraceback (most recent call last):")
+print("  File \"<stdin>\", line 1, in <module>")
+print("TypeError: 'str' object does not support item assignment")
+
+print("\ncreation of a new String")
+print("\nrun the  command--> word[:2]+'new'")
+print (word[:2]+'new')
+
+print("\nprinting word")
+print (word)
+
+print("\nlength of word is")
+print (len(word))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Examples/2_Lists.py
@@ -0,0 +1,95 @@
+#tutorials 3: Lists
+
+#more examples 1- using print function
+print(
+	"Start of Lists example"
+	)
+
+Lists=[1,2,3,4]
+print("Lists -->")
+print(Lists)
+
+#using indexes in List
+print("if we want to get a character in a particular index we can use the below code ")
+print("Lists[2] --> ")
+print(Lists[2])
+
+print("if we want to get a character in a reverse range use -ve numbers in index, here 4 has an index -1")
+print("Lists[-1] --> ")
+print(Lists[-1])
+
+print("if we want to get characters in a range use : , in this case we are trying to get characters from -2 position until end, hence we dont specify any number after :")
+print("Lists[-2:] --> ")
+print(Lists[-2:])
+
+print("if we dont specify starting range and end range, entire list will be printed")
+print("Lists[:] --> ")
+print(Lists[:])
+
+print("\nif we have to add few more sets of numbers to the existing List, it can be done as follows ")
+print("Lists+[5,6,7,8] --> ")
+print(Lists+[5,6,7,8])
+
+print("\n assigning the result to a new list")
+print("Lists2=Lists+[5,6,7,8] --> ")
+Lists2=Lists+[5,6,7,8]
+print(Lists2)
+
+print("\n assign a new value in nth index Lists2[n]= x  -> ")
+Lists2[4]=5.5
+
+
+print("\n Lists2, notice the changed value 5.5 --> ")
+print(Lists2)
+
+print("\n length of lists--> len(List name) ")
+print(Lists)
+print (len(Lists))
+
+print("\n clearing a List --> ListName[:]=[] ")
+Lists[:]=[]
+print(Lists)
+
+print("\n Nested Lists ")
+ListA=[1,2,3]
+print("\nListA--> ")
+print(ListA)
+
+ListB=[4,5,6]
+print("\nListB--> ")
+print(ListB)
+
+ListAB=[ListA,ListB]
+print("\nListAB = ListA, ListB --> ")
+print(ListAB)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
