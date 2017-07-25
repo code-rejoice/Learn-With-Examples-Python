f2690003401ffde5548004598d60534767c5da39
Code Rejoice <code.rejoice@gmail.com>
2017-07-25 06:42:16 -0400

    if condition examples
    Examples with if, if else, if elif else are included

---
+++ Examples/3_If.py
@@ -0,0 +1,50 @@
+#tutorials 4: if, if else, if elif else
+
+print(
+	"-->if, if else, if else elif examples"
+	)
+	
+#if conditon
+print("--> if example"
+	"\nAssign x=10 and check using if condition\n"
+	"x = 10\n"
+	"if x == 10:\n"
+	)
+x = 10
+if x == 10:
+		print("result: x is equal to 10\n")
+		
+#if else condition
+print(
+	"-->If else example"
+	)
+print(
+	"\nAssign x=10 and check using if condition\n"
+	"x = 9\n"
+	"if x == 10:\n"
+	)
+x = 9
+if x == 10:
+		print("result: x is equal to 10\n")
+else:
+		print("result: x is NOT EQUAL to 10\n")
+		
+		
+#if elif else condition		
+print(
+	"-->If elif else example\n"
+	"\nTake input from user\n"
+	)
+x = int(input("enter an integer: \n"))
+
+print(
+	"The value of entered number 'x' is",x
+	)
+
+if x > 0:
+		print("\nresult: x =", x,", is greater than zero \n")
+elif x < 0:
+		print("\nresult: x =", x,", is less than zero\n")
+else: 
+	print("\nEntered digit is 0\n")
+	
