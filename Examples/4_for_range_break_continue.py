225029c07205162ceabff5e7b9476358d60b3a28
Code Rejoice <code.rejoice@gmail.com>
2017-07-25 21:57:46 -0400

    for range break continue examples

--- Examples/4_for_range_break_continue.py
+++ Examples/4_for_range_break_continue.py
@@ -20,4 +20,40 @@ print("\nnumbers List-->",numbers,"\n")
 
 for anything in numbers:
 	print(anything,"\t",len(anything))
+	
+#RANGE
+print("-->range example\n")
+print("range is used to iterate over a sequence of numbers\n "
+" -->for i in range(5):\n")
+for i in range(5):
+	print("the value of i is ",i)
 
+#RANGE WITH INCREMENTS
+print("\n specify the starting and end range along with a increment value\n"
+	"in this case, the value increments 3 each time until it reaches 9\n")
+for i in range(0, 10, 3):
+	print("the value of i is ",i)	
+	
+print("\n"
+	"each time -3 is added to the previous negative number which starts from -1\n")	
+for i in range(-1, -20, -3):
+	print("the value of i is ",i)	
+	
+
+#using break in a for loop
+print("\n--> break example")
+for i in range(-1, -20, -3):
+	if i == -7:
+		print("the value of i is ",i," loop ends\n")	
+		break
+	else: 
+		print("the value of i is ",i)
+	
+#using continue in a for loop
+print("\n--> continue example")
+for i in range(-1, -20, -3):
+	if i == -7:
+		print("the value of i is ",i," loop continues with else as well\n")	
+		continue
+	else: 
+		print("the value of i is ",i)	
