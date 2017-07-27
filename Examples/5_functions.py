8abac170192eb8e7474bac091a4e85000e181287
Code Rejoice <code.rejoice@gmail.com>
2017-07-26 21:26:16 -0400

    updated function code

--- Examples/5_functions.py
+++ Examples/5_functions.py
@@ -15,4 +15,32 @@ def vowelTest(alphabet):
 		print("TEST FAILED: the given alphabet is NOT a vowel")
 
 print("calling function test(alphabet) \n")
-vowelTest(x.lower())		
+vowelTest(x.lower())	
+
+# second method to call a function
+
+print("\n--> second method to call a function-->")
+y = str(input("\nenter second alphabet: \n"))
+print ("entered alphabet is ",y)
+vowelTestSecond=vowelTest(y.lower())
+vowelTestSecond
+
+#third method to cal a function
+def vowelTestThird(alphabet,PASS="TEST PASS: the given alphabet is a vowel",
+FAIL="TEST FAILED: the given alphabet is NOT a vowel"):
+	vow=['a','e','i','o','u']
+	if any(alphabet in s for s in vow) :
+		print(PASS)
+	else:
+		print(FAIL)
+		
+print("\n--> third method to call a function-->")
+z = str(input("\nenter third alphabet: \n"))
+print ("entered alphabet is ",z)
+vowelTestThird(z.lower())
+vowelTestThird(z.lower(),"\nfunction with optional param called: " 
+"RESULT PASS: try with 25 other alphabets!",
+"\nfunction with optional param called: "
+"FAIL: dont worry! you have 25 more alphabets to test!")
+
+#TO BE CONTINUED
